b172b5fd0ffb15aa2c83d301dd4a10a2f93a2519
Office <72687971+OxygenClub@users.noreply.github.com>
2024-12-15 00:08:32 +0200

    Create models.lua

---
+++ models.lua
@@ -0,0 +1,146 @@
+return {
+    Tank = {
+        asset = "rbxassetid://7234164026",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Spongebob = {
+        asset = "rbxassetid://2053546933",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Palm Tree"] = {
+        asset = "rbxassetid://2874534251",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Computer = {
+        asset = "rbxassetid://7337360846",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Toilet = {
+        asset = "rbxassetid://13759230824",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Car = {
+        asset = "rbxassetid://6320186829",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Jet Plane"] = {
+        asset = "rbxassetid://5265234147",
+        offset = Vector3.new(0, 0, 0),
+    },
+    House = {
+        asset = "rbxassetid://6884551990",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Spongebob House"] = {
+        asset = "rbxassetid://2180401070",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Squidward House"] = {
+        asset = "rbxassetid://5443285986",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Treatment Chair"] = {
+        asset = "rbxassetid://6204632403",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Minecraft Chunk"] = {
+        asset = "rbxassetid://12599187294",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Ferb = {
+        asset = "rbxassetid://9700088049",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Jevil = {
+        asset = "rbxassetid://7519820628",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Warehouse = {
+        asset = "rbxassetid://14171833145",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Garfield = {
+        asset = "rbxassetid://4803338779",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Devil Mansion"] = {
+        asset = "rbxassetid://725124009",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Moai = {
+        asset = "rbxassetid://14508125871",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Bike = {
+        asset = "rbxassetid://6100916461",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Lebron James Cube"] = {
+        asset = "rbxassetid://17067132241",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Bath Tub"] = {
+        asset = "rbxassetid://442356090",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Formula 1"] = {
+        asset = "rbxassetid://6091320618",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["SCP-173"] = {
+        asset = "rbxassetid://7176615381",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["L4D Tank"] = {
+        asset = "rbxassetid://4790861792",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Slenderman = {
+        asset = "rbxassetid://15338650899",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Skibidi Camera Man"] = {
+        asset = "rbxassetid://13952379795",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Police = {
+        asset = "rbxassetid://5754591094",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Huey Helicopter"] = {
+        asset = "rbxassetid://7214308452",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Skibidi Toilet"] = {
+        asset = "rbxassetid://101895754871911",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Bonnie = {
+        asset = "rbxassetid://3155669494",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Bonnie = {
+        asset = "rbxassetid://3155669494",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Bed = {
+        asset = "rbxassetid://6965669511",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Bunk Bed"] = {
+        asset = "rbxassetid://16330800942",
+        offset = Vector3.new(0, 0, 0),
+    },
+    Door = {
+        asset = "rbxassetid://481276508",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Gold Bar"] = {
+        asset = "rbxassetid://9329082430",
+        offset = Vector3.new(0, 0, 0),
+    },
+    ["Golf Ball"] = {
+        asset = "rbxassetid://15838437235",
+        offset = Vector3.new(0, 0, 0),
+    },
+}
